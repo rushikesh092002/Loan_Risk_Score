3f0726e2887ed09b6f1123964c70c9cf1ac66376
Rushikesh0912 <rushikeshgaikhe09@gmail.com>
2024-11-16 21:26:38 +0530

    processor and data

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,58 @@
+import sys
+import os
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
+@dataclass
+
+class DataIngestionConfig:
+    train_data_path=os.path.join("artifacts","train.csv")
+    test_data_path=os.path.join("artifacts","test.csv")
+    raw_data_path=os.path.join("artifacts",'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.data_ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("into initiate data ingestion")
+        try:
+            df=pd.read_csv("notebook\data\Loan.csv")
+            logging.info("dataset is load")
+
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.data_ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("train and test initaliized")
+
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.data_ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.data_ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("training and testing is done")
+
+            return(
+                self.data_ingestion_config.train_data_path,
+                self.data_ingestion_config.test_data_path
+            )
+
+        except Exception  as e:
+            raise CustomException(e,sys)
+
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
